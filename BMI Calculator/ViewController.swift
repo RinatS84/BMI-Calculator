916659d5c5c472cc83e36dc2aa7c1784fc548d18
Rinat <rinmur2002@gmail.com>
2024-06-13 21:38:08 +0300

    added BackroundImage

--- BMI Calculator/ViewController.swift
+++ BMI Calculator/ViewController.swift
@@ -2,11 +2,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let calculateBackgroundImageView: UIImageView = {
+       let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: "calculate_background")
+        return imageView
+    }()
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .blue
+        constraints()
+    }
+    
+    func constraints() {
+        
+        view.addSubview(calculateBackgroundImageView)
+        
+        NSLayoutConstraint.activate([
+            calculateBackgroundImageView.topAnchor.constraint(equalTo: view.topAnchor),
+            calculateBackgroundImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            calculateBackgroundImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            calculateBackgroundImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ])
     }
 
 
